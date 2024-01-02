b0ca3092110d54d2f6b9dd4eff8d50721413a80d
Andrew Ivanov <and@hfrog.ru>
2024-01-02 15:00:09 +0300

    Add ssh

--- scripts/prepare_vms_for_k8s.sh
+++ scripts/prepare_vms_for_k8s.sh
@@ -5,5 +5,11 @@ set -e
 aws s3 --endpoint-url=https://storage.yandexcloud.net cp s3://$BUCKET/terraform/terraform-output.json .
 
 mkdir ~/.ssh
+echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_ed25519
+chmod 400 ~/.ssh/id_ed25519
+
 VMS=$(jq -r .external_ip.value[] terraform-output.json)
-echo $VMS
+for f in $VMS; do
+  echo $f
+  ssh -o 'StrictHostKeyChecking=no' ubuntu@$f ip -br -c a
+done
