2b3bb03cc80f2f896768efa040e14f0a6bfe423f
Andrew Ivanov <and@hfrog.ru>
2024-01-13 17:14:17 +0300

    Install cilium

--- scripts/local_kubeadm_init.sh
+++ scripts/local_kubeadm_init.sh
@@ -12,12 +12,25 @@ function wait_for_readiness {
   done || true
 }
 
+function install_cilium_client {
+  CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
+  CLI_ARCH=$(uname -m)
+  if [ "$CLI_ARCH" = "x86_64" ]; then CLI_ARCH=amd64; fi
+  curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+  sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+  sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
+  rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+}
+
 kubeadm init --pod-network-cidr=10.244.0.0/16 --control-plane-endpoint $APISERVER_IP:6443
 
 export KUBECONFIG=/etc/kubernetes/admin.conf
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-
-cp /etc/kubernetes/admin.conf ~ubuntu && chown ubuntu: ~ubuntu/admin.conf
+install_cilium_client
+cilium install --version 1.14.5
+cilium status --wait
 
 wait_for_readiness
 kubectl get nodes -o wide
+
+# prepare admin.conf for downloading and then uploading to the Yandex S3 cloud
+cp /etc/kubernetes/admin.conf ~ubuntu && chown ubuntu: ~ubuntu/admin.conf && chmod go-rw ~ubuntu/admin.conf
