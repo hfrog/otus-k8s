191b65d26e6e90f616cb6aa4ea0044f6876490a1
Andrew Ivanov <and@hfrog.ru>
2024-01-15 16:40:38 +0300

    Move K8S_VERSION var to file

--- scripts/environment_vars.sh
+++ scripts/environment_vars.sh
@@ -1,2 +1,5 @@
 # External apiserver port
 export API_SERVER_PORT=6443
+
+# Kubernetes version
+export K8S_VERSION=1.28
--- scripts/prepare_vms_for_k8s.sh
+++ scripts/prepare_vms_for_k8s.sh
@@ -2,6 +2,8 @@
 set -x
 set -e
 
+. scripts/environment_vars.sh
+
 aws s3 --endpoint-url=https://storage.yandexcloud.net cp s3://$BUCKET/terraform/terraform-output.json .
 VMS=$(jq -r '.external_ip.value|with_entries(select(.key|startswith("lb-") == false))|.[]' terraform-output.json)
 for f in $VMS; do
