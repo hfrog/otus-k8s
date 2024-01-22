fe22f570a9b7afbaec4dd8957c5cf4c2a23584f9
Andrew Ivanov <and@hfrog.ru>
2024-01-22 22:03:00 +0300

    Test github actions env file

---
+++ scripts/app_prepare_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+# set -x  # Don't show commands because of secrets
+
+aws s3 --endpoint-url=https://storage.yandexcloud.net cp s3://$BUCKET/k8s/admin.conf .
+WERF_KUBECONFIG_BASE64=$(base64 -w 0 < admin.conf)
+echo "::add-mask::$WERF_KUBECONFIG_BASE64"
+echo "WERF_KUBECONFIG_BASE64=$WERF_KUBECONFIG_BASE64" >> $GITHUB_OUTPUT
