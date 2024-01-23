c2086507a1c420b2f3bc257f66d757536ce78f64
Andrew Ivanov <and@hfrog.ru>
2024-01-23 21:32:45 +0300

    Use werf charts

--- scripts/app_prepare_env.sh
+++ scripts/app_prepare_env.sh
@@ -6,5 +6,3 @@ aws s3 --endpoint-url=https://storage.yandexcloud.net cp s3://$BUCKET/k8s/admin.
 WERF_KUBECONFIG_BASE64=$(base64 -w 0 < admin.conf)
 echo "::add-mask::$WERF_KUBECONFIG_BASE64"
 echo "WERF_KUBECONFIG_BASE64=$WERF_KUBECONFIG_BASE64" >> $GITHUB_ENV
-
-cat app/environment_vars.sh >> $GITHUB_ENV
