ab9c260cc81e6dc8920bd10198ac8bec9f14de73
Andrew Ivanov <and@hfrog.ru>
2024-01-26 11:39:58 +0300

    Make LB address in ingress dynamic

--- scripts/app_prepare_env.sh
+++ scripts/app_prepare_env.sh
@@ -6,3 +6,7 @@ aws s3 --endpoint-url=https://storage.yandexcloud.net cp s3://$BUCKET/k8s/admin.
 WERF_KUBECONFIG_BASE64=$(base64 -w 0 < admin.conf)
 echo "::add-mask::$WERF_KUBECONFIG_BASE64"
 echo "WERF_KUBECONFIG_BASE64=$WERF_KUBECONFIG_BASE64" >> $GITHUB_ENV
+
+aws s3 --endpoint-url=https://storage.yandexcloud.net cp s3://$BUCKET/terraform/terraform-output.json .
+LB1_EXTERNAL_IP=$(jq -r '.external_ip.value."lb-1"' terraform-output.json)
+echo "LB1_EXTERNAL_IP=$LB1_EXTERNAL_IP" >> $GITHUB_ENV
