c108a2a9cafdfe70ffe23a58045320886ead3b86
Andrew Ivanov <and@hfrog.ru>
2024-01-28 05:17:00 +0300

    Add terraform.tfvars

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,7 @@
+
+# "fd866d9q7rcg6h4udadk" # ubuntu-22-04-lts-v20231225
+image_id     = "fd85an6q1o26nf37i2nl" # ubuntu-20-04-lts-v20231218
+
+lb_count     = 1
+master_count = 1
+worker_count = 2
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,4 @@
 variable "image_id" {
-  default = "fd85an6q1o26nf37i2nl" # ubuntu-20-04-lts-v20231218
-#  default = "fd866d9q7rcg6h4udadk" # ubuntu-22-04-lts-v20231225
 }
 
 variable "ssh-keys" {
@@ -8,15 +6,12 @@ variable "ssh-keys" {
 }
 
 variable "master_count" {
-  default = 1
 }
 
 variable "worker_count" {
-  default = 2
 }
 
 variable "lb_count" {
-  default = 1
 }
 
 variable "lb_ip_pool" {
